cc06d4e822949c7630bf7217c20cb3f2b013a659
T.Ligniere <45320522+TLigniere@users.noreply.github.com>
2023-12-20 17:13:38 +0100

    Update boto_config.py

--- salt/backup_python/boto_config.py
+++ salt/backup_python/boto_config.py
@@ -3,7 +3,7 @@ import glob
 import os
 from datetime import datetime
 
-connection = boto3.client('s3', aws_access_key_id='6G2M8JG62IKT0MEICI8O', aws_secret_access_key='I8UJFI9UZDPBFP4COXMIBY40Z7YYBWULDYHTWGZ7', region_name='eu-west-2', endpoint_url='https://oos.eu-west-2.outscale.com')
+connection = boto3.client('s3', aws_access_key_id='', aws_secret_access_key='', region_name='eu-west-2', endpoint_url='')
 list_of_files = glob.glob('/usr/local/bin/backup_sql/*') # * means all if need specific format then *.csv
 latest_file = max(list_of_files, key=os.path.getctime)
 dt= datetime.now()
