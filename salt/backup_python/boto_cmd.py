babb58ee803d472b3a77259705686c2996c50f2e
T.Ligniere <45320522+TLigniere@users.noreply.github.com>
2023-12-20 17:14:05 +0100

    Update boto_cmd.py

--- salt/backup_python/boto_cmd.py
+++ salt/backup_python/boto_cmd.py
@@ -1,6 +1,6 @@
 import boto3
 
-connection = boto3.client('s3', aws_access_key_id='6G2M8JG62IKT0MEICI8O', aws_secret_access_key='I8UJFI9UZDPBFP4COXMIBY40Z7YYBWULDYHTWGZ7', region_name='eu-west-2', endpoint_url='https://oos.eu-west-2.outscale.com')
+connection = boto3.client('s3', aws_access_key_id='', aws_secret_access_key='', region_name='', endpoint_url='')
 
 response=connection.list_objects(
         Bucket="timothee-bucket"
